8c233c6f578dfe538e92976eb7e38da60db94b97
Juan Escalada <juanescalada175@gmail.com>
2023-11-09 11:18:52 -0800

    Update build.gradle.kts

--- Lecture9/app/build.gradle.kts
+++ Lecture9/app/build.gradle.kts
@@ -50,6 +50,8 @@ android {
 }
 
 dependencies {
+    // Add this to use Jetpack Compose Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.5")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
