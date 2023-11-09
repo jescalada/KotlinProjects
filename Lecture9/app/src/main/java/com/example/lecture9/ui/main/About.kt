508f40d2c123a9671edaf1749e913f013bf63f41
Juan Escalada <juanescalada175@gmail.com>
2023-11-09 11:18:14 -0800

    Add About page

---
+++ Lecture9/app/src/main/java/com/example/lecture9/ui/main/About.kt
@@ -0,0 +1,21 @@
+package com.example.lecture9.ui.main
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun About() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "About", fontSize = 30.sp)
+    }
+}
