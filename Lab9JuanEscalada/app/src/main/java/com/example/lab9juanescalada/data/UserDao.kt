b13d9c8a41cdc84408545d305ae026ce8cb55c1d
Juan Escalada <juanescalada175@gmail.com>
2023-11-22 08:43:01 -0800

    Add UserDao.kt

---
+++ Lab9JuanEscalada/app/src/main/java/com/example/lab9juanescalada/data/UserDao.kt
@@ -0,0 +1,18 @@
+package com.example.lab9juanescalada.data
+
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
+interface UserDao {
+    // Unpack the user into name and email
+    // Insert the user into the database
+    @Query("INSERT INTO users (name, email) VALUES (:name, :email)")
+    fun insertUser(name: String, email: String)
+    @Query("SELECT * FROM users")
+    fun getAllUsers(): List<LocalUser>
+    @Query("SELECT * FROM users WHERE uid = :id")
+    fun getUser(id: Int): LocalUser
+    @Query("DELETE FROM users WHERE uid = :id")
+    fun deleteUser(id: Int)
+}
