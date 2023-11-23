e7ef5eafecee52378264edb2e980628aab73d8b5
Juan Escalada <juanescalada175@gmail.com>
2023-11-23 11:47:29 -0800

    Update Lab9 SignupState.kt

--- Lab9JuanEscalada/app/src/main/java/com/example/lab9juanescalada/ui/main/SignupState.kt
+++ Lab9JuanEscalada/app/src/main/java/com/example/lab9juanescalada/ui/main/SignupState.kt
@@ -5,6 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 
 class SignupState {
+    var uid by mutableStateOf<Int?>(null)
+    val onUIDChange: (String) -> Unit = {
+        uid = it.toIntOrNull()
+    }
     var name by mutableStateOf("")
     val onNameChange: (String) -> Unit = { name = it }
     var email by mutableStateOf("")
@@ -13,5 +17,5 @@ class SignupState {
         // Check if email is valid using a simple regex
         validEmail = it.matches(Regex("[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\\.[a-zA-Z0-9_-]+"))
     }
-    var validEmail by mutableStateOf(false)
+    var validEmail by mutableStateOf(true)
 }
