3692560dbde498f2291b4b404c246aa8924ee8f9
Juan Escalada <juanescalada175@gmail.com>
2023-11-30 10:56:18 -0800

    Add Card flipping functionality

--- Lecture11/app/src/main/java/com/example/pokeinfo/MainActivity.kt
+++ Lecture11/app/src/main/java/com/example/pokeinfo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pokeinfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -10,17 +11,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.pokeinfo.data.Pokemon
 import com.example.pokeinfo.ui.main.PokemonState
-import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,10 +37,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             val pokemonState = PokemonState(pokemonRepository)
             LaunchedEffect(pokemonState) {
-                for (i in 1..9) {
-                    val id = Random.nextInt(1, 1017)
-                    pokemonState.addPokemonToList(pokemonState.getPokemon(id))
-                }
+                pokemonState.loadRandomPokemon()
             }
             MainContent(pokemonState)
         }
@@ -52,29 +56,65 @@ fun MainContent(pokemonState: PokemonState) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PokemonCard(pokemon: Pokemon) {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(48.dp, 4.dp)
-            .border(BorderStroke(4.dp, Color(0xFFFF0000)))
-            .background(color = Color(0xFFFFDADA))
-            .padding(8.dp),
-        horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally) {
-        Text(text = pokemon.name, style = MaterialTheme.typography.headlineMedium, textAlign = androidx.compose.ui.text.style.TextAlign.Center)
-        // Render the image using the URL from the Pokemon object
-        AsyncImage(
-            model = parseImageUrl(pokemon.sprites),
-            contentDescription = pokemon.name
-        )
-        Text(text = "Types: ${pokemon.types.joinToString(", ") { it.type["name"]?.uppercase()!! }}", style = MaterialTheme.typography.bodyMedium)
+    var isFlipped by remember {
+        mutableStateOf(false)
     }
+
+    Surface(onClick = {isFlipped = !isFlipped}, modifier = Modifier.animateContentSize()) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(48.dp, 4.dp)
+                .border(BorderStroke(4.dp, Color(0xFFFF0000)))
+                .background(color = Color(0xFFFFDADA), RoundedCornerShape(16.dp))
+                .padding(8.dp),
+            horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
+        ) {
+            if (!isFlipped) {
+                CardFront(pokemon)
+            } else {
+                CardBack(pokemon)
+            }
+        }
+    }
+}
+
+@Composable
+fun CardFront(pokemon: Pokemon) {
+    Text(
+        text = pokemon.name,
+        style = MaterialTheme.typography.headlineMedium,
+        textAlign = androidx.compose.ui.text.style.TextAlign.Center
+    )
+    // Render the image using the URL from the Pokemon object
+    AsyncImage(
+        model = parseImageUrl(pokemon.sprites),
+        contentDescription = pokemon.name
+    )
+    Text(
+        text = "Types: ${pokemon.types.joinToString(", ") { it.type["name"]?.uppercase()!! }}",
+        style = MaterialTheme.typography.bodyLarge
+    )
+}
+
+@Composable
+fun CardBack(pokemon: Pokemon) {
+    Text(
+        text = pokemon.name,
+        style = MaterialTheme.typography.headlineMedium
+    )
+    Text(text = "Stats", style = MaterialTheme.typography.headlineSmall)
+    Text(
+        text = pokemon.stats.joinToString("\n") { "${it.stat["name"]?.uppercase()!!} = ${it.base_stat}" },
+        style = MaterialTheme.typography.bodyMedium
+    )
 }
 
 fun parseImageUrl(dict: Map<String, Any>): String {
     val other = dict["other"] as Map<String, Any>
     val officialArtwork = other["official-artwork"] as Map<String, Any>
-    val frontDefault = officialArtwork["front_default"] as String
-    return frontDefault
+    return officialArtwork["front_default"] as String
 }
