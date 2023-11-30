f09d01e4961f2409609c7ae26802f34ca13baf1f
Juan Escalada <juanescalada175@gmail.com>
2023-11-29 19:41:34 -0800

    Add Lecture11 API

--- Lecture11/app/src/main/java/com/example/lecture11/data/ApiEndpoints.kt
+++
@@ -1,7 +0,0 @@
-package com.example.lecture11.data
-
-enum class ApiEndpoints(val url: String) {
-    BASE_URL("https://api.artic.edu/api/v1/"),
-    ARTWORKS("${BASE_URL}/artworks"),
-    FIELDS1("${ARTWORKS.url}?fields=id,title,image_id")
-}
